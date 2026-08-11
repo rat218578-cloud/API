541005594c13555fbe7ae607fd6fe396936ac44c
rat218578-cloud <rat218578@gmail.com>
2026-08-11 18:56:16 -0300

    Update apigames_service.py

--- apigames_service.py
+++ apigames_service.py
@@ -22,7 +22,7 @@ class ApiGamesService:
         self.last_signal_id = None
         self.running = False
         self.email = None
-        self.source = "immersivevip"
+        self.source = "immersive"
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
             'Accept': 'application/json, text/plain, */*',
